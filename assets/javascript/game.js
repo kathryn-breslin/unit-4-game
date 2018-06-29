e7ad8bd8a40b7fdf0700eb1eea4cf1ff444724f9
Kat Bres <kathryn.l.breslin@gmail.com>
2018-06-29 11:28:32 -0500

    CSS updates and Reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,18 @@
-//Randomly generate number between 19 and 120
-//crystals are given a random number between 1 and 12
-//When you click on a crystal, the score increases by a certain number, depending on the crystal clicked
-//the score will increase with whatever crystal is pressed
-//if you are able to increase you score to the same amount as the randomly generated number, wins increases by 1 and you are told "You won!"
-//if you go over the number amount, losses increases by 1 and you are told "you lost!"
-
-
-//declare variables 
 $(document).ready(function() {
 
+//declare variables
 setRandomNumber ();
 wins = 1;
 losses = 1;
 yourScore = 0;
 
+//Setting the HTML
 $('#wins').text("Wins: ");
 $('#losses').text("Losses: ");
 $('.yourScoreIs').text("Your Score Is: ");
 $('#yourScore').text("Score");
 
+//Random number
 function setRandomNumber() {
     randomNumber = Math.floor(Math.random () * 121);
     console.log(randomNumber);
@@ -27,10 +21,8 @@ function setRandomNumber() {
 
 var crystalNumbers = [12, 1, 6, 5];
 
-for (var i = 0; i < crystalNumbers.length; i++) {
-   // crystalImage = $('.crystal-image');
-   // crystalImage.attr("data-crystalvalue", crystalNumbers[i]);
-    
+//Setting the Crystals
+for (var i = 0; i < crystalNumbers.length; i++) { 
    var crystalImage = $("<img>");
         crystalImage.addClass("crystal-image");
         crystalImage.attr("src", './assets/images/crytal_1.png');
@@ -40,11 +32,13 @@ for (var i = 0; i < crystalNumbers.length; i++) {
         $("#crystals").append(crystalImage);
 }
 
+//Click
 $('.crystal-image').on('click', function() {
     crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     yourScore += crystalValue;
     $('#yourScore').text(yourScore);
+    $('#scoreMessage').html('');
 
     if (yourScore === randomNumber) {
         $('#wins').html("Wins: " + wins++);
@@ -60,27 +54,15 @@ $('.crystal-image').on('click', function() {
  
     })
 
+//Resetting the game
+
 function reset() {
     yourScore = 0;
     setRandomNumber ();
     $('#yourScore').html('');
     $('#randomNumber').text(randomNumber);
-    $('#scoreMessage').html('');
+    
+    
 }
 
 });
-
-
-//Resetting the game
-
-
-
-//});
-
-//Resetting the game
-
-
-//$('.yourScoreIs').html("<h5> Your Score Is: </h5>");
-//$('#yourScore').html("<h3>" + yourScore + "</h3>");
-//$('#wins').html("<h4> Wins: " + wins + "</h4>");
-//$('#losses').html("<h4> Losses: ") + losses + "</h4>");
